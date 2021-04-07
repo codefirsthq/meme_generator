f1b3df5fc9d3311baedc4183171d3b8ce46b8313
kopianan <ananalfredcarlos@gmail.com>
2021-04-07 16:15:57 +0700

    Adding meme generator banner

--- lib/presentation/dashboard/dashboard_page.dart
+++ lib/presentation/dashboard/dashboard_page.dart
@@ -8,6 +8,42 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                height: 200,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.amber,
+                    image: DecorationImage(
+                      colorFilter: ColorFilter.linearToSrgbGamma(),
+                      fit: BoxFit.fitWidth,
+                      image: AssetImage('assets/images/funny_dog.jpg'),
+                    )),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Meme Generator",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 50),
+                    ),
+                    Text("don't forget to laugh, release your emotions")
+                  ],
+                ),
+              ),
+              Text("Anak 2"),
+              Text("Anak 3"),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
