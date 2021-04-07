3e70228e22ee37b390b984d91da54bac5346626c
kopianan <ananalfredcarlos@gmail.com>
2021-04-07 16:30:24 +0700

    Exercise Solution, Separate Widget Of Dashboard page

--- lib/presentation/dashboard/dashboard_page.dart
+++ lib/presentation/dashboard/dashboard_page.dart
@@ -38,12 +38,63 @@ class _DashboardPageState extends State<DashboardPage> {
                   ],
                 ),
               ),
-              Text("Anak 2"),
-              Text("Anak 3"),
+              BuildCardFeature(
+                assetImage: 'assets/images/meme1.jpg',
+                title: "VERSION\n#1",
+              ),
+              BuildCardFeature(
+                assetImage: 'assets/images/meme2.jpg',
+                title: "VERSION\n#2",
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class BuildCardFeature extends StatelessWidget {
+  const BuildCardFeature({
+    Key key,
+    @required this.assetImage,
+    @required this.title,
+  }) : super(key: key);
+
+  final String assetImage;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        height: 180,
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey[400],
+                blurRadius: 3,
+                spreadRadius: 3,
+                offset: Offset(5, 5))
+          ],
+          borderRadius: BorderRadius.circular(8),
+          image: DecorationImage(
+            image: AssetImage(assetImage),
+            fit: BoxFit.fitWidth,
+          ),
+          color: Colors.white,
+        ),
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          alignment: Alignment.centerRight,
+          child: Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white54),
+          ),
+        ));
   }
 }
